cc0a873af3c491e8a82c33fe2b2ebff7747fe8bc
Andreas Kloeckner <inform@tiker.net>
2014-05-03 14:38:13 -0300

    Initial version

---
+++ ready-notebooks.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+
+set -e
+set -x
+
+mkdir -p upload
+
+for nb in [0-9]*/*ipynb; do
+  echo "PROCESSING $nb"
+  DIR="$(dirname "$nb")"
+
+  CONV_DIR="upload/$DIR"
+  mkdir -p "$CONV_DIR"
+  CONV_BASE="upload/${nb%.ipynb}"
+  CONV_PY="${CONV_BASE}.py"
+  CONV_HTML="${CONV_BASE}.html"
+
+  ./demo-ready-ipynb --keep "$nb" "${CONV_BASE}.ipynb"
+  if test "$nb" -nt "$CONV_PY"; then
+    ipython nbconvert "$nb" --to=python "--output=${CONV_BASE}"
+  fi
+  if test "$nb" -nt "$CONV_HTML"; then
+    ipython nbconvert "$nb" --to=html "--output=${CONV_BASE}"
+  fi
+
+  CONV_DIR="cleared/$DIR"
+  mkdir -p "$CONV_DIR"
+  CONV_IPYNB="cleared/$nb"
+  ./demo-ready-ipynb "$nb" "$CONV_IPYNB"
+done
