fcc4ec093f57ba32991866f3853b483299724790
Andreas Kloeckner <inform@tiker.net>
2014-05-06 10:11:29 -0300

    Use processed notebook for conversion

--- ready-notebooks.sh
+++ ready-notebooks.sh
@@ -19,12 +19,13 @@ for nb in [0-9]*/*ipynb; do
   CONV_PY="${CONV_BASE}.py"
   CONV_HTML="${CONV_BASE}.html"
 
-  "$MYDIR/demo-ready-ipynb" --keep "$nb" "${CONV_BASE}.ipynb"
+  PROCESSED_IPYNB="${CONV_BASE}.ipynb"
+  "$MYDIR/demo-ready-ipynb" --keep "$nb" "$PROCESSED_IPYNB"
   if test "$nb" -nt "$CONV_PY"; then
-    ipython nbconvert "$nb" --to=python "--output=${CONV_BASE}"
+    ipython nbconvert "$PROCESSED_IPYNB" --to=python "--output=${CONV_BASE}"
   fi
   if test "$nb" -nt "$CONV_HTML"; then
-    ipython nbconvert "$nb" --to=html "--output=${CONV_BASE}"
+    ipython nbconvert "$PROCESSED_IPYNB" --to=html "--output=${CONV_BASE}"
   fi
 
   CONV_DIR="cleared/$DIR"
