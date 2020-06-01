debe170a7903b287bc71a93d570441ca47796e05
Andreas Kloeckner <inform@tiker.net>
2020-05-31 19:36:20 -0500

    Teach prepare-all-notebooks.sh about separate input and output directories

--- prepare-all-notebooks.sh
+++ prepare-all-notebooks.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /usr/bin/env zsh
 
 set -e
 setopt -o EXTENDED_GLOB
@@ -9,15 +9,32 @@ function with_echo()
   "$@"
 }
 
-unset PYTHONWARNINGS
+function mkdir_and_cp()
+{
+  dn=$(dirname "$2")
+  mkdir -p "$dn"
+  with_echo cp "$1" "$2"
+}
 
-mkdir -p upload
+unset PYTHONWARNINGS
 
 ME=$(readlink -f "$0")
 DIR=$(dirname "$ME")
 MYDIR=$(cd "$DIR" && pwd)
 
-for nb in */**/*.ipynb; do
+if [[ "$1" = "" ]]; then
+  INPUTDIR="$(pwd)"
+else
+  INPUTDIR="$1"
+fi
+
+if [[ "$2" = "" ]]; then
+  OUTPUTDIR="$(pwd)"
+else
+  OUTPUTDIR="$2"
+fi
+
+for nb in $INPUTDIR/*/**/*.ipynb; do
   if [[ $nb == upload* ]]; then
     continue
   fi
@@ -26,17 +43,21 @@ for nb in */**/*.ipynb; do
   fi
 
   DIR="$(dirname "$nb")"
+  BN="$(basename "$nb")"
+  RELDIR="$(realpath --relative-to="$INPUTDIR" "$DIR")"
+
+  CONV_DIR="$OUTPUTDIR/upload/$RELDIR"
+  CONV_BASE="$CONV_DIR/${BN%.ipynb}"
+  CONV_PY="${CONV_BASE}.py"
+  CONV_HTML="${CONV_BASE}.html"
+  PROCESSED_IPYNB="${CONV_BASE}.ipynb"
+
   if test -f "$DIR/.do-not-publish"; then
     continue
   fi
 
-  CONV_DIR="upload/$DIR"
   mkdir -p "$CONV_DIR"
-  CONV_BASE="upload/${nb%.ipynb}"
-  CONV_PY="${CONV_BASE}.py"
-  CONV_HTML="${CONV_BASE}.html"
 
-  PROCESSED_IPYNB="${CONV_BASE}.ipynb"
   if ! test -f "$PROCESSED_IPYNB" || test "$nb" -nt "$PROCESSED_IPYNB"; then
     with_echo "$MYDIR/prepare-ipynb" remove-marks "$nb" "$PROCESSED_IPYNB"
   fi
@@ -47,32 +68,28 @@ for nb in */**/*.ipynb; do
     with_echo python -m nbconvert "$PROCESSED_IPYNB" --to=html
   fi
 
-  CONV_DIR="cleared/$DIR"
+  CONV_DIR="$OUTPUTDIR/upload/$RELDIR"
   mkdir -p "$CONV_DIR"
-  CONV_IPYNB="cleared/$nb"
+  CONV_IPYNB="$CONV_DIR/$BN"
   with_echo "$MYDIR/prepare-ipynb" clear-output clear-marked-inputs "$nb" "$CONV_IPYNB"
 done
-function mkdir_and_cp()
-{
-  dn=$(dirname "$2")
-  mkdir -p "$dn"
-  with_echo cp "$1" "$2"
-}
-for i in */**/*~*ipynb~*.pyc~*\~(#q.)(#qN); do
+
+for i in $INPUTDIR/*/**/*~*ipynb~*.pyc~*\~(#q.)(#qN); do
   DIR="$(dirname "$i")"
+  RELNAME="$(realpath --relative-to="$INPUTDIR" "$i")"
   if test -f "$DIR/.do-not-publish"; then
     continue
   fi
 
-  if [[ $i == upload* ]]; then
+  if [[ "$RELNAME" == upload* ]]; then
     continue
   fi
-  if [[ $i == ipython-demo-tools* ]]; then
+  if [[ "$RELNAME" == ipython-demo-tools* ]]; then
     continue
   fi
-  if [[ $i == cleared* ]]; then
+  if [[ "$RELNAME" == cleared* ]]; then
     continue
   fi
-  with_echo mkdir_and_cp $i cleared/$i
-  with_echo mkdir_and_cp $i upload/$i
+  with_echo mkdir_and_cp "$i" "$OUTPUTDIR/cleared/$RELNAME"
+  with_echo mkdir_and_cp "$i" "$OUTPUTDIR/upload/$RELNAME"
 done
